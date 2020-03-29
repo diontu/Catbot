d82550bdae7a41c790d4ef82821510e765e3dc21
Dion <diontugit@gmail.com>
2020-03-28 21:48:46 -0400

    added cat command

--- commands/cat.js
+++ commands/cat.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 module.exports = {
     name: 'cat',
     aliases: ['catpic', 'catpicture'],
@@ -6,6 +8,14 @@ module.exports = {
     guildOnly: true,
     args: false,
     execute (message, args) {
-
+        const catpics = fs.readdirSync('./catpics').filter(catpic => {
+            return catpic.endsWith('.png') || catpic.endsWith('.jpeg') || catpic.endsWith('jpg')
+        }); 
+        var randomIndex = Math.floor(Math.random() * catpics.length);
+        if (catpics.length === 0) {
+            message.channel.send("Meow (I don't have anymore cat pics for you)...");
+        }
+        message.channel.send(`${catpics.length}`);
+        message.channel.send("Meow meow (here's a cutie)!", {files: [`./catpics/${catpics[randomIndex]}`]});
     }
 };
